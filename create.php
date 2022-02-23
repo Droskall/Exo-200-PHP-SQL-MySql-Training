32754ecd62b227c82290f41a6985b4409c38b572
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-02-23 13:06:07 +0100

    fini

--- create.php
+++ create.php
@@ -5,21 +5,24 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Randonnées, ajout</title>
+    <link rel="stylesheet" href="css/basics.css">
 </head>
 <body>
-    <form action="">
-        <input type="text" name="name" placeholder="Nom de la randonnée">
-        <select name="difficulty" id="difficulty">
+    <form method="post" action="./public/createHiking.php">
+        <input type="text" name="name" placeholder="Nom de la randonnée" required>
+
+        <select name="difficulty" id="difficulty" required>
             <option value="très facile">Très facile</option>
             <option value="facile">Facile</option>
             <option value="moyen">Moyen</option>
             <option value="difficile">Difficile</option>
             <option value="très difficile">Très difficile</option>
         </select>
-        <input type="number" name="distance">
-        <!-- Ajoutez un / des champs pour gérer la donnée de type time à insérer via PHP -->
-        .....
-        <input type="number" name="height_difference">
+
+        <input type="number" name="distance" placeholder="Distance (km)" required>
+        <input type="number" name="duration" placeholder="Temps en minutes" required>
+        <input type="number" name="height_difference" placeholder="Dénivelé (m)" required>
+        <input id="enter" type="submit" name="Enter" value="ajouter">
     </form>
 </body>
 </html>
--- delete.php
+++ delete.php
@@ -1,1 +1,23 @@
 <?php
+try {
+    $server = "localhost";
+    $db = "exo_200";
+    $user = "root";
+    $psw = "";
+
+    $bdd = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $psw);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION) .
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM hiking WHERE id = $id";
+    if ($bdd->exec($sql) !== false) {
+        echo "l'id " . $id . " a bien été supprimé !";
+        header("Location: read.php");
+    }
+
+}
+catch (PDOException $e) {
+    echo $e->getMessage();
+}
